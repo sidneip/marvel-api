2f8fc512457e3f38d13a45c0aa8639f2ddf252e1
Sidnei Pacheco <sidnei.junior@hubfintech.com.br>
2016-11-17 12:05:33 -0200

    characters controller

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,10 +1,12 @@
 class Api::CharactersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   before_action :set_character, only: [:show, :edit, :update, :destroy]
 
   # GET /api/characters
   # GET /api/characters.json
   def index
     @characters = Character.all
+    render json: @characters
   end
 
   # GET /api/characters/1
@@ -18,7 +20,7 @@ class Api::CharactersController < ApplicationController
   def create
     @character = Character.new(character_params)
     if @character.save
-      render json: @character
+      render json: @character, status: 201
     else
       render json: @character.errors, status: :unprocessable_entity
     end
@@ -48,6 +50,10 @@ class Api::CharactersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_character
       @character = Character.find(params[:id])
+    end
+
+    def not_found
+      render json: { error: "not_found".to_json }, status: 404
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- test/controllers/api/characters_controller_test.rb
+++ test/controllers/api/characters_controller_test.rb
@@ -10,6 +10,18 @@ class Api::CharactersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should return characters' do
+    get api_characters_url, as: :json
+    body = JSON.parse @response.body
+    assert Character.all.size, body.size
+  end
+
+  test 'should render character first in json respose' do
+    get api_characters_url, as: :json
+    body = JSON.parse @response.body
+    assert Character.first.name, body[0]['name']
+  end
+
   test "should create api_character" do
     assert_difference('Character.count') do
       post api_characters_url, params: { character: { name: 'teste', description: 'abc' } }, as: :json
@@ -18,14 +30,39 @@ class Api::CharactersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should show api_character" do
+  test  "should create new character" do
+    total = Character.all.size
+    post api_characters_url, params: { character: {name: 'teste2121', description: 'a1'} }, as: :json
+    assert_equal total+1, Character.all.size
+    assert_equal 'teste2121', Character.last.name
+    assert_response 201
+  end
+
+  test "should show character" do
     get api_character_url(@character), as: :json
     assert_response :success
   end
 
-  test "should update api_character" do
-    patch api_character_url(@character), params: { character: { name: 'abc' } }, as: :json
+  test "should render character" do
+    get api_character_url(@character), as: :json
+    body = JSON.parse @response.body
+    assert_equal @character.id, body["id"]
+    assert_equal @character.name, body["name"]
+  end
+
+  test "should return 404 if character not exist" do
+    get api_character_url('asd1'), as: :json
+    assert_response :not_found
+  end
+
+  test "should update character response success" do
+    patch api_character_url(@character), params: { character: { name: 'updatename' } }, as: :json
     assert_response 201
+  end
+
+  test "should update name" do
+    patch api_character_url(@character), params: { character: { name: 'testeupdate' } }, as: :json
+    assert_equal 'testeupdate', @character.reload.name
   end
 
   test "should destroy api_character" do
