278dece8cf4356416bde0da61fb184db404359d0
Sidnei Pacheco <sidneip.junior@gmail.com>
2016-11-17 23:40:36 -0200

    null session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   rescue_from AuthError, :with => :invalid_key
   private
   
