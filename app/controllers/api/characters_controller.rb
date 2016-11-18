7a67610134b0d83253030630593c36f45f2a1667
Sidnei Pacheco <sidneip.junior@gmail.com>
2016-11-18 00:54:38 -0200

    add parameters

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -64,6 +64,6 @@ class Api::CharactersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def character_params
-      params.require(:character).permit(:name, :description)
+      params.require(:character).permit(:name, :description, :thumbnail, :code)
     end
 end
